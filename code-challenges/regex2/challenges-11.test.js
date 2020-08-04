d9c34fa9112a3110be817df66274e3eeac93565d
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-08-03 21:53:18 -0600

    finished 1-3 on 11

--- code-challenges/regex2/challenges-11.test.js
+++ code-challenges/regex2/challenges-11.test.js
@@ -12,10 +12,12 @@ Note the space in between first and last names.
 You can assume that neither firstName nor lastName will be blank
 ------------------------------------------------------------------------------------------------ */
 const toLastNames = people => {
-  return people.map(value => {
-    return
+  let newArr = [];
+
+  people.map(person => {
+    return newArr.push(`${person.firstName} ${person.lastName}`);
   })
-  // Solution code here...
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -27,7 +29,8 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePin = (pin) => {
-  // Solution code here...
+  let validator = /^\d{4}\b/g;
+    return validator.test(pin);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -47,7 +50,8 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  let emailRegex = /^\w+(.\w+)?@\w+[.](net|com|org)\b/gi
+  return emailRegex.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -72,7 +76,8 @@ Return either true or false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePhoneNumber = (phoneNumber) => {
-  // Solution code here...
+  let phoneRegex = /^(\(\d{3}\)|\d{3})(\s|-)?\(\d{3})(\s|-)?(\d{4})$/gm
+  return phoneRegex.test(phoneNumber);
 };
 
 /* ------------------------------------------------------------------------------------------------
