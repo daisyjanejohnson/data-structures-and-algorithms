7c8348080fc7f8de0acfb7e72573e9fda435a6bd
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-08-11 23:57:21 -0600

    finished whiteboard and code but cannot get tests to run

--- 401challenges/__tests__/arrayShift.test.js
+++ 401challenges/__tests__/arrayShift.test.js
@@ -1,4 +1,4 @@
-const insertShiftArray = require('../401challenges/__tests__/arrayShift.js');
+const insertShiftArray = require('../401challenges/arrayShift/arrayShift.js');
 
 test('insertShiftArray exists', () => {
   expect(shiftArray).toBeDefined();
@@ -6,21 +6,21 @@ test('insertShiftArray exists', () => {
 
 //length test
 test('length should increase by 1', () => {
-  const startArray = ['x', 'x', 'x', 'x', 'x', 'x'];
-  const resultArray = insertShiftArray(startArray, 'y');
+  const startArray = [4, 8 ,15, 23, 42];
+  const resultArray = insertShiftArray(startArray, 16);
   expect(resultArray.length).toBe(startArray.length+1);
 });
 
 it('should not modify the original', () => {
-  const startArray = ['x', 'x', 'x', 'x', 'x', 'x'];
-  insertShiftArray(startArray, 'y');
+  const startArray = [4, 8 ,15, 23, 42];
+  const resultArray = insertShiftArray(startArray, 16);
   expect(resultArray.length).toBe(startArray.length+1);
 })
 //Is Y in the correct spot
 it('should have item added in correct spot', () => {
-  const startArray = ['x', 'x', 'x', 'x', 'x', 'x'];
-  const resultArray = insertShiftArray(startArray, 'y');
-  expect(resultArray[3]).toBe('y');
+  const startArray = [4, 8 ,15, 23, 42];
+  const resultArray = insertShiftArray(startArray, 16);
+  expect(resultArray[3]).toBe(16);
 })
 
 /*
